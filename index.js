7a2850b537ea1678e0c42b54f786fb014f017627
Ahmed Raza <99830227+ahmedraza41190@users.noreply.github.com>
2023-03-06 21:19:55 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+function foo(a){
+    var t = document.getElementById('inp')
+    t.value += a 
+}
+function res (){
+    var a = document.getElementById('inp')
+    a.value = eval(a.value)
+}
+function clr(){
+    document.getElementById('inp').value = '0'
+}
